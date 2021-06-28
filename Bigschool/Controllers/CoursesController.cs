2c8a898cd005dd90a87705125d859a5f976f4ee1
Admin <Admin@DESKTOP-G822FHT>
2021-06-28 16:48:27 +0700

    update

---
+++ Bigschool/Controllers/CoursesController.cs
@@ -0,0 +1,42 @@
+﻿using Bigschool.Models;
+using Bigschool.ViewModel;
+using Microsoft.AspNet.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Bigschool.Controllers
+{
+    public class CoursesController : Controller
+    {
+        private readonly ApplicationDbContext _dbContext;
+        // GET: Courses
+        public CoursesController()
+        {
+            _dbContext = new ApplicationDbContext();
+            
+        }
+        [Authorize]
+        [HttpPost]
+        public ActionResult Create(CourseViewModel viewModel)
+        {
+            if(!ModelState.IsValid)
+            {
+                viewModel.categories = _dbContext.categories.ToList();
+                return View("Create", viewModel);
+            }
+            var course = new Course
+            {
+                LecturerId=User.Identity.GetUserId(),
+                Datetime= viewModel.GetDateTime(),
+                CategoryId= viewModel.Category,
+                Place =viewModel.Place
+            };
+            _dbContext.Courses.Add(course);
+            _dbContext.SaveChanges();
+            return RedirectToAction("Index","Home");
+        }
+    }
+}
---
+++ Bigschool/ViewModel/CourseViewModel.cs
@@ -0,0 +1,25 @@
+﻿using Bigschool.Models;
+using Microsoft.Owin.Security;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace Bigschool.ViewModel
+{
+    public class CourseViewModel
+    {
+        [Required]
+        public string Place { get; set; }
+        public string Date { get; set; }
+        public string Time { get; set; }
+        public byte Category { get; set; }
+        public IEnumerable<Category> categories { get; set; }
+        public DateTime GetDateTime()
+        {
+            return DateTime.Parse(string.Format("{0} {1}", Date, Time));
+        }
+
+    }
+}
